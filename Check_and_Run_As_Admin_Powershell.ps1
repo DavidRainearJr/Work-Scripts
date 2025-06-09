2c92976276dbebee0544348fc482ae4b81579dd2
David Rainear Jr <32674936+DavidRainearJr@users.noreply.github.com>
2025-06-09 19:43:53 -0400

    Update Check_and_Run_As_Admin_Powershell.ps1
    New script added

--- Check_and_Run_As_Admin_Powershell.ps1
+++ Check_and_Run_As_Admin_Powershell.ps1
@@ -6,3 +6,11 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
    Start-Process powershell -ArgumentList "-File `"$PSCommandPath`"" -Verb RunAs
    exit
 }
+
+#New ------------------------
+# Auto-elevate if not running as admin
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+   Write-Host "Restarting as Administrator..."
+   Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+   exit
+}
