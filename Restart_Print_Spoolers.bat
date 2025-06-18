edf8b2cb9f8f0a1764d5255e2a4825e1e1d98544
David Rainear Jr <32674936+DavidRainearJr@users.noreply.github.com>
2025-06-18 00:15:10 -0400

    Update Restart_Print_Spoolers.bat

--- Restart_Print_Spoolers.bat
+++ Restart_Print_Spoolers.bat
@@ -1,44 +1,53 @@
 @echo off
 cls
 echo Which Matchplay Spooler do you want to reboot?
-echo 1, 2, 3, 4, Cage, or All
+echo 1, 2, 3, 4, Cage, or All:
+
 :: Using CHOICE to take input with options
 choice /c 1234CA /m "Selection:"
+
 :: ERRORLEVEL returns a value based on the order of /c options
+
 IF ERRORLEVEL 6 (
-   echo You selected All
-   sc.exe \\DPRO1 stop spooler && sc.exe \\DPRO1 start spooler
-   sc.exe \\DPRO7 stop spooler && sc.exe \\DPRO7 start spooler
-   sc.exe \\DPRO5 stop spooler && sc.exe \\DPRO5 start spooler
-   sc.exe \\DPRO11 stop spooler && sc.exe \\DPRO11 start spooler
-   echo Cage must be done on the [REDACTED] server.   User: [REDACTED] Pass: [REDACTED]
-   GOTO END
+    echo You selected All
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    echo Cage must be done on the [REDACTED] server.         User: [REDACTED] Pass: [REDACTED]
+    GOTO END
 )
+
 IF ERRORLEVEL 5 (
-   echo You selected Cage
-   echo This is to be done on the [REDACTED] server.   User: [REDACTED] Pass: [REDACTED]
-   GOTO END
+    echo You selected Cage
+    echo This is to be done on the [REDACTED] server.         User: [REDACTED] Pass: [REDACTED]
+    GOTO END
 )
+
 IF ERRORLEVEL 4 (
-   echo You selected 4
-   sc.exe \\DPRO11 stop spooler && sc.exe \\DPRO11 start spooler
-   GOTO END
+    echo You selected 4
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    GOTO END
 )
+
 IF ERRORLEVEL 3 (
-   echo You selected 3
-   sc.exe \\DPRO5 stop spooler && sc.exe \\DPRO5 start spooler
-   GOTO END
+    echo You selected 3
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    GOTO END
 )
+
 IF ERRORLEVEL 2 (
-   echo You selected 2
-   sc.exe \\DPRO7 stop spooler && sc.exe \\DPRO7 start spooler
-   GOTO END
+    echo You selected 2
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    GOTO END
 )
+
 IF ERRORLEVEL 1 (
-   echo You selected 1
-   sc.exe \\DPRO1 stop spooler && sc.exe \\DPRO1 start spooler
-   GOTO END
+    echo You selected 1
+    sc.exe \\[REDACTED] stop spooler && sc.exe \\[REDACTED] start spooler
+    GOTO END
 )
+
 :END
 echo Operation completed.
 pause
